4c3ef7a3caec471b51866289cd959940f0950719
Brandon <brandonroberge@hotmail.com>
2018-12-08 00:12:12 -0500

    Day 1

---
+++ Day1/Program.fs
@@ -0,0 +1,21 @@
+﻿module Program
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    let linesSeq = System.IO.File.ReadLines("input.txt") |> Seq.map (fun x -> x |> int)
+    let sum = linesSeq |> Seq.sum
+    
+    let firstRepeatingFrequency =
+        seq { 
+            while true do
+                yield! linesSeq
+        }
+        |> Seq.scan (fun (set, sum) i -> (Set.add sum set, sum + i)) (Set.empty, 0)
+        |> Seq.find (fun (set, sum) -> Set.contains sum set)
+        |> snd
+        
+    printfn "%d" sum
+    printfn "%d" firstRepeatingFrequency
+    0
